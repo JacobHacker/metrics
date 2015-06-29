3256e6d5823ffe8dc467228ccbd509269f8eaa84
Jacob Hacker <jacobhacker@outlook.com>
2015-06-28 20:30:39 -0500

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		watch: {
+			scripts: {
+				files: ['web/**'],
+			},
+			options: {
+				livereload: true
+			},
+		},
+		connect: {
+			server: {
+				options: {
+					port: 9001,
+					base: 'root'
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+
+	grunt.registerTask('default', ['connect', 'watch']);
+};
---
+++ web/js/graph.js
@@ -0,0 +1,4 @@
+$( document ).ready(function() {
+	d3.select("body").append("p").text("New paragraph!");
+	console.log('woo');
+});
